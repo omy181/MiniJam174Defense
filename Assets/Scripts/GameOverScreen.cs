2e95a6f2232bd0bedc02d5029b06736a4fe5cb3f
HOLYPERSON <31584910+omy181@users.noreply.github.com>
2024-12-23 01:21:38 +0300

    hopefully last save

--- Assets/Scripts/GameOverScreen.cs
+++ Assets/Scripts/GameOverScreen.cs
@@ -8,29 +8,26 @@ using UnityEngine.UI;
 public class GameOverScreen : MonoBehaviour
 {
     [SerializeField] private Button _restartButton;
-    [SerializeField] private TMP_Text _titleText;
-    [SerializeField] private GameObject _panel;
+    [SerializeField] private Button _restartButton2;
+    [SerializeField] private GameObject _gameover;
+    [SerializeField] private GameObject _win;
     void Start()
     {
         _restartButton.onClick.AddListener(LevelManager.instance.RestartLevel);
-        _panel.gameObject.SetActive(false);
+        _restartButton2.onClick.AddListener(LevelManager.instance.RestartLevel);
+        _gameover.gameObject.SetActive(false);
+        _win.gameObject.SetActive(false);
     }
 
     public void OnGameOver(bool isWin)
     {
-        _panel.gameObject.SetActive(true);
-        if (isWin)
-        {
-            _titleText.text = "You Did it !";
-        }
-        else
-        {
-            _titleText.text = "You Couldn't Reached Your Destination";
-        }
+        _gameover.gameObject.SetActive(!isWin);
+        _win.gameObject.SetActive(isWin);
     }
 
     public void HideScreen()
     {
-        _panel.gameObject.SetActive(false);
+        _gameover.gameObject.SetActive(false);
+        _win.gameObject.SetActive(false);
     }
 }
